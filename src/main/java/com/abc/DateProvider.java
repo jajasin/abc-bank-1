02d7d248b5772dcd72d7d0d9b46bde2993e78727
jajasin <aajasin@hotmail.com>
2015-06-15 02:49:18 -0400

    Update DateProvider.java

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -1,18 +1,18 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Calendar;
 
 public class DateProvider {
-    private static DateProvider instance = null;
+    private static final DateProvider instance = new DateProvider();
+
+    private DateProvider() {
+    }
 
     public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
         return instance;
     }
 
     public Date now() {
         return Calendar.getInstance().getTime();
     }
-}
